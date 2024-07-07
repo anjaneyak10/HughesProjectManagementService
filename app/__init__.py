9f7a8ea8ba0c433b639ce78a228e68efcd71c248
Anjaneya <anjaneyak10@gmail.com>
2024-07-07 16:51:29 -0400

    adding port

--- app/__init__.py
+++ app/__init__.py
@@ -6,14 +6,7 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    DATABASE_URI = os.getenv('DATABASE_URL')
-    print(SECRET_KEY)
-    print(DATABASE_URI)
     app.config.from_object('app.config.Config')
-
     init_db(app)
-
     app.register_blueprint(auth_bp, url_prefix='/project')
-
     return app
